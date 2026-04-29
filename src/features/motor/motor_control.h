6bcf122bb6adfcbe059c1813e2c4bcd42edbb793
Isobel <isobelmjones1@gmail.com>
2026-04-29 12:38:19 +1000

    add duty restraints to motor_control.h

--- src/features/motor/motor_control.h
+++ src/features/motor/motor_control.h
@@ -8,12 +8,17 @@
 #define MOTOR_DECEL_LIMIT_RPM_PER_S 500
 #define MOTOR_ESTOP_DECEL_RPM_PER_S 1000
 
+#define MOTOR_DUTY_MIN 0
+#define MOTOR_DUTY_START 10
+#define MOTOR_DUTY_MAX 45
+
 void initMotorControl(void);
 
 uint16_t motorRampUpdate(uint16_t desiredSpeedRPM,  bool estopActive, float dtSeconds);
-
 uint16_t motorControlGetReferenceSpeed(void);
 void motorControlResetReferenceSpeed(void);
 
+
+uint16_t motorSpeedToDutyPlaceholder(uint16_t referenceSpeedRPM); // swap to actualSpeed for hall sensor RPM !!
 
 #endif
